c7f99f7cac9757a682986e04c33fe13c004fa3fc
Daniel <69821353+DanielAcevedo99@users.noreply.github.com>
2024-05-23 10:42:42 -0500

    Update app.module.ts

--- parcial2-daniel-acevedo/src/app.module.ts
+++ parcial2-daniel-acevedo/src/app.module.ts
@@ -1,26 +1,36 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { EstudianteModule } from './estudiante/estudiante.module';
-import { ProfesorModule } from './profesor/profesor.module';
-import { PropuestaModule } from './propuesta/propuesta.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ProyectoModule } from './proyecto/proyecto.module';
+import { ProfesorModule } from './profesor/profesor.module';
+import { EstudianteModule } from './estudiante/estudiante.module';
+import { PropuestaModule } from './propuesta/propuesta.module';
+import { Proyecto } from './proyecto/proyecto.entity';
+import { Profesor } from './profesor/profesor.entity';
+import { Estudiante } from './estudiante/estudiante.entity';
+import { Propuesta } from './propuesta/propuesta.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
+  imports: [ProyectoModule, ProfesorModule, EstudianteModule, ProfesorModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: 'daniel',  
-      password: 'uniandes',  
-      database: 'proyecto',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      username: 'postgres',
+      password: 'postgres',
+      database: 'museum',
+      entities: [Proyecto, Profesor, Estudiante, Propuesta],
+      dropSchema: true,
       synchronize: true,
+      keepConnectionAlive: true
     }),
     EstudianteModule,
     ProfesorModule,
     PropuestaModule,
-    ProyectoModule,
+    ProyectoModule,,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
